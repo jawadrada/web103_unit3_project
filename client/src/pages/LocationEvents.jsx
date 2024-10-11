b1dc6ba84dd59c31b1145e363c74b4bc8e460f2e
jawadrada <jawadrada18@gmail.com>
2024-10-11 16:41:56 -0400

    Updated Project

--- client/src/pages/LocationEvents.jsx
+++ client/src/pages/LocationEvents.jsx
@@ -1,44 +1,31 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';  // Import useParams to get locationId
-import { getEventsByLocationId } from '../services/EventsAPI';  // Import your service function
+import { useParams } from 'react-router-dom';
+import { getEventsByLocationId } from '../services/EventsAPI';
 import Event from '../components/Event';
 import '../css/LocationEvents.css';
 
 const LocationEvents = () => {
-  const { locationId } = useParams();  // Get locationId from route params
-  const [location, setLocation] = useState({});
+  const { id } = useParams(); // Get location ID from the URL
   const [events, setEvents] = useState([]);
 
   useEffect(() => {
-    const fetchLocationEvents = async () => {
+    const fetchEvents = async () => {
       try {
-        const eventsData = await getEventsByLocationId(locationId);  // Fetch events for this location
+        const eventsData = await getEventsByLocationId(id); // Use the location ID
         setEvents(eventsData);
       } catch (error) {
         console.error('Error fetching events:', error);
       }
     };
 
-    fetchLocationEvents();
-  }, [locationId]);  // Fetch events when locationId changes
+    fetchEvents();
+  }, [id]);
 
   return (
-    <div className='location-events'>
-      <header>
-        <div className='location-image'>
-          {/* Replace with your image logic */}
-          <img src={location.image} alt={location.name} />
-        </div>
-
-        <div className='location-info'>
-          <h2>{location.name}</h2>
-          <p>{location.address}, {location.city}, {location.state} {location.zip}</p>
-        </div>
-      </header>
-
+    <div className="location-events">
       <main>
-        {
-          events.length > 0 ? events.map(event => (
+        {events && events.length > 0 ? (
+          events.map((event) => (
             <Event
               key={event.id}
               id={event.id}
@@ -47,13 +34,17 @@ const LocationEvents = () => {
               time={event.time}
               image={event.image}
             />
-          )) : <h2>No events scheduled at this location yet!</h2>
-        }
+          ))
+        ) : (
+          <h2>
+            <i className="fa-regular fa-calendar-xmark fa-shake"></i> No events
+            scheduled at this location yet!
+          </h2>
+        )}
       </main>
     </div>
   );
 };
 
 export default LocationEvents;
-
 
